0481015052edbf5d3e4bce7b2173efb519db21da
Chris Lehneis <cdlehneis@gmail.com>
2015-04-12 15:15:54 -0400

    adds javascript animations

--- js/main.js
+++ js/main.js
@@ -1,5 +1,36 @@
 $( document ).ready(function() {
+	$('.splash.home').css('display', 'block');
+	$('.splash.home').addClass('fade-in');
+
+
     $('.nav-btn').on('click', function(){
-    	$('.splash').addClass('slide');
+    	var navBtn = $(this).attr('id');
+    	$('.splash').addClass('fade-out');
+		switch(navBtn) {
+		    case 'home':
+		    	$('.splash').hide();
+		    	$('.splash.home').removeClass('fade-out');
+		    	$('.splash.home').show();
+		        $('.splash.home').addClass('fade-in');
+		        break;
+		    case 'about':
+		    	$('.splash').hide();
+		       	$('.splash.about').removeClass('fade-out');
+		       	$('.splash.about').show();
+		        $('.splash.about').addClass('fade-in');
+		        break;
+		    case 'portfolio':
+		    	$('.splash').hide();
+		    	$('.splash.portfolio').removeClass('fade-out');
+		    	$('.splash.portfolio').show();
+		        $('.splash.portfolio').addClass('fade-in');
+		        break;
+		    case 'contact':
+		    	$('.splash').hide();
+		    	$('.splash.contact').removeClass('fade-out');
+		    	$('.splash.contact').show();
+		        $('.splash.contact').addClass('fade-in');
+		        break;
+		}
     });
 });
