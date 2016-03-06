2dbc224947210620b17752dae994429c4d79f04c
Chris Lehneis <cdlehneis@gmail.com>
2016-03-05 21:14:16 -0500

    whatevs

--- js/main.js
+++ js/main.js
@@ -10,36 +10,18 @@ $( document ).ready(function() {
 		switch(navBtn) {
 		    case 'home':
 		    	$('.splash, .cntnr').hide();
-                // $('#map-canvas').css('visibility', 'hidden');
-                // $('#map-street-canvas').css('visibility', 'hidden');
-                // $('#map-canvas').css('z-index', '0');
-                // $('#map-street-canvas').css('z-index', '0');
-                // $('#map-canvas').removeClass('fade-in');
-                // $('#map-street-canvas').removeClass('fade-in');
 		    	$('.splash.home, .cntnr.home').removeClass('fade-out');
 		    	$('.splash.home, .cntnr.home').show();
 		        $('.splash.home, .cntnr.home').addClass('fade-in');
 		        break;
 		    case 'about':
 		    	$('.splash, .cntnr').hide();
-                // $('#map-canvas').css('visibility', 'hidden');
-                // $('#map-street-canvas').css('visibility', 'hidden');
-                // $('#map-canvas').css('z-index', '0');
-                // $('#map-street-canvas').css('z-index', '0');
-                // $('#map-canvas').removeClass('fade-in');
-                // $('#map-street-canvas').removeClass('fade-in');
 		       	$('.splash.about, .cntnr.about').removeClass('fade-out');
 		       	$('.splash.about, .cntnr.about').show();
 		        $('.splash.about, .cntnr.about').addClass('fade-in');
 		        break;
 		    case 'portfolio':
 		    	$('.splash, .cntnr').hide();
-                // $('#map-canvas').css('visibility', 'visible');
-                // $('#map-street-canvas').css('visibility', 'visible');
-                // $('#map-canvas').css('z-index', '30');
-                // $('#map-street-canvas').css('z-index', '30');
-                // $('#map-canvas').addClass('fade-in');
-                // $('#map-street-canvas').addClass('fade-in');
 		    	$('.splash.portfolio, .cntnr.portfolio').removeClass('fade-out');
 		    	$('.splash.portfolio, .cntnr.portfolio').show();
 		        $('.splash.portfolio, .cntnr.portfolio').addClass('fade-in');
@@ -64,25 +46,6 @@ $( document ).ready(function() {
 		e.stopPropagation();
 	});
 
-    // $('.cntnr.about').on('click', function(){
-    // 	$('.pagnation').removeClass('fill');
-    // 	if($('.name.harsimran').css('display') === 'block'){
-    // 		$('.harsimran').hide();
-    // 		$('.gunit').fadeIn();
-    // 		$('.g').addClass('fill');
-    // 	}
-    // 	else if($('.name.gunit').css('display') === 'block'){
-    // 		$('.gunit').hide();
-    // 		$('.pavit').fadeIn();
-    // 		$('.p').addClass('fill');
-    // 	}
-    // 	else if($('.name.pavit').css('display') === 'block'){
-    // 		 $('.pavit').hide();
-    // 		$('.harsimran').fadeIn();
-    // 		$('.h').addClass('fill');
-    // 	}
-    // });
-
     $('.tab.commercial').addClass('selected');
 
     $('.tab').on('click', function(){
@@ -104,19 +67,5 @@ $( document ).ready(function() {
         $('iframe').css('visibility', 'hidden');
         $('.'+ id).css('visibility', 'visible');
 
-    });
-
-    // $('.tab').hover(function(){
-    //     var tab = $(this);
-    //     $('.tab').css({
-    //         'background-color' : 'none',
-    //         'color' : 'black'
-    //     });
-    //     tab.css({
-    //         'background-color' : 'black',
-    //         'color' : 'white',
-    //         'opacity' : '0.8'
-    //     });
-    // });
-	
+    });	
 });
